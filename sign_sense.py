ecf99897c74aeb70f16024e5e1f0b0278c3056b4
sanjeevz3009 <sanjeevz1717@gmail.com>
2024-04-11 23:54:33 +0100

    Neural networks training algorithms complete

--- sign_sense.py
+++ sign_sense.py
@@ -52,8 +52,6 @@ def draw_landmarks_custom(image, results):
                                   mediapipe_draw.DrawingSpec(color=(245, 66, 230), thickness=1, circle_radius=1)
                                   )
 
-    return image
-
 # capture = cv2.VideoCapture(0)
 # # Access/ set media pipe mode
 # with mediapipe_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
@@ -91,51 +89,51 @@ gestures = np.array(["Hello", "Thanks", "Iloveyou", "Good"])
 no_sequences = 30
 sequence_length = 30
 
-for gesture in gestures:
-    for sequence in range(no_sequences):
-        try:
-            os.makedirs(os.path.join(data_location, gesture, str(sequence)))
-        except:
-            pass
+# for gesture in gestures:
+#     for sequence in range(no_sequences):
+#         try:
+#             os.makedirs(os.path.join(data_location, gesture, str(sequence)))
+#         except:
+#             pass
 
-capture = cv2.VideoCapture(0)
-# Access/ set media pipe mode
-with mediapipe_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
-    # Iterate through all the gestures we need to train
-    for gesture in gestures:
-        # Next mediapipe_holistic through the sequences/ videos
-        for sequence in range(no_sequences):
-            #mediapipe_holistic through the video length
-            for frame_count in range(sequence_length):
-                ret, frame = capture.read()
+# capture = cv2.VideoCapture(0)
+# # Access/ set media pipe mode
+# with mediapipe_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+#     # Iterate through all the gestures we need to train
+#     for gesture in gestures:
+#         # Next mediapipe_holistic through the sequences/ videos
+#         for sequence in range(no_sequences):
+#             #mediapipe_holistic through the video length
+#             for frame_count in range(sequence_length):
+#                 ret, frame = capture.read()
 
-                # Make detections
-                image, results = mediapipe_detection(frame, holistic)
-                print(results)
+#                 # Make detections
+#                 image, results = mediapipe_detection(frame, holistic)
+#                 print(results)
 
-                # Draw landmarks
-                image = draw_landmarks_custom(image, results)
+#                 # Draw landmarks
+#                 image = draw_landmarks_custom(image, results)
 
-                # Delay for the next frame
-                if frame_count == 0:
-                    cv2.putText(image, "The program will now start to collect training data", (120, 200), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 4, cv2.LINE_AA)
-                    cv2.putText(image, f"Frames being collected for {gesture} video number {sequence}", (15, 12), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
-                    cv2.waitKey(2000)
-                else:
-                    cv2.putText(image, f"Frames being collected for {gesture} video number {sequence}", (15, 12), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
+#                 # Delay for the next frame
+#                 if frame_count == 0:
+#                     cv2.putText(image, "The program will now start to collect training data", (120, 200), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 4, cv2.LINE_AA)
+#                     cv2.putText(image, f"Frames being collected for {gesture} video number {sequence}", (15, 12), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
+#                     cv2.waitKey(2000)
+#                 else:
+#                     cv2.putText(image, f"Frames being collected for {gesture} video number {sequence}", (15, 12), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
                 
-                # Retrieve and export gesture key points
-                get_key_points = extract_landmarks(results)
-                numpy_path = os.path.join(data_location, gesture, str(sequence), str(frame_count))
-                np.save(numpy_path, get_key_points)
+#                 # Retrieve and export gesture key points
+#                 get_key_points = extract_landmarks(results)
+#                 numpy_path = os.path.join(data_location, gesture, str(sequence), str(frame_count))
+#                 np.save(numpy_path, get_key_points)
 
-                cv2.imshow("Feed", image)
+#                 cv2.imshow("Feed", image)
 
-                if cv2.waitKey(1) == ord("q"):
-                    break
+#                 if cv2.waitKey(1) == ord("q"):
+#                     break
 
-    capture.release()
-    cv2.destroyAllWindows()
+#     capture.release()
+#     cv2.destroyAllWindows()
 
 label_map = {label:num for num, label in enumerate(gestures)}
 
@@ -159,10 +157,12 @@ tensor_board_callback = TensorBoard(log_dir=log_location)
 
 model = Sequential()
 model.add(LSTM(64, return_sequences=True, activation="relu", input_shape=(30, 1662)))
-model.add(LSTM(64, return_sequences=True, activation="relu"))
+model.add(LSTM(128, return_sequences=True, activation="relu"))
 model.add(LSTM(64, return_sequences=False, activation="relu"))
 
 model.add(Dense(64, activation="relu"))
 model.add(Dense(32, activation="relu"))
-
 model.add(Dense(gestures.shape[0], activation="softmax"))
+
+model.compile(optimizer="Adam", loss="categorical_crossentropy", metrics=["categorical_accuracy"])
+model.fit(x_train, y_train, epochs=1000, callbacks=[tensor_board_callback])
