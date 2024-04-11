ed85b86f9a98c8e95b92de57523d9694d34e243d
sanjeevz3009 <sanjeevz1717@gmail.com>
2024-04-12 00:33:10 +0100

    Training input collection method, fixed and neural networks model complete

--- sign_sense.py
+++ sign_sense.py
@@ -85,7 +85,8 @@ def extract_landmarks(results):
 data_location = os.path.join("mediapipe_data")
 
 # Actions/ sign language gestures to detect
-gestures = np.array(["Hello", "Thanks", "Iloveyou", "Good"])
+# gestures = np.array(["Hello", "Thanks", "Iloveyou", "Good"])
+gestures = np.array(["Hello", "Iloveyou", "Good"])
 no_sequences = 30
 sequence_length = 30
 
@@ -109,25 +110,24 @@ sequence_length = 30
 
 #                 # Make detections
 #                 image, results = mediapipe_detection(frame, holistic)
-#                 print(results)
 
 #                 # Draw landmarks
-#                 image = draw_landmarks_custom(image, results)
+#                 draw_landmarks_custom(image, results)
 
 #                 # Delay for the next frame
 #                 if frame_count == 0:
 #                     cv2.putText(image, "The program will now start to collect training data", (120, 200), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 4, cv2.LINE_AA)
-#                     cv2.putText(image, f"Frames being collected for {gesture} video number {sequence}", (15, 12), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
+#                     cv2.putText(image, f"Frames being collected for {gesture} video number {sequence}", (15, 12), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
+#                     cv2.imshow("Feed", image)
 #                     cv2.waitKey(2000)
 #                 else:
-#                     cv2.putText(image, f"Frames being collected for {gesture} video number {sequence}", (15, 12), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
-                
+#                     cv2.putText(image, f"Frames being collected for {gesture} video number {sequence}", (15, 12), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
+#                     cv2.imshow("Feed", image)
+
 #                 # Retrieve and export gesture key points
 #                 get_key_points = extract_landmarks(results)
 #                 numpy_path = os.path.join(data_location, gesture, str(sequence), str(frame_count))
 #                 np.save(numpy_path, get_key_points)
-
-#                 cv2.imshow("Feed", image)
 
 #                 if cv2.waitKey(1) == ord("q"):
 #                     break
@@ -166,3 +166,7 @@ model.add(Dense(gestures.shape[0], activation="softmax"))
 
 model.compile(optimizer="Adam", loss="categorical_crossentropy", metrics=["categorical_accuracy"])
 model.fit(x_train, y_train, epochs=1000, callbacks=[tensor_board_callback])
+
+model.predict(x_test)
+model.summary()
+
