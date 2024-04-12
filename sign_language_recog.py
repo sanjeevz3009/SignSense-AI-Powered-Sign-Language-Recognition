350ec0a461b149f172e6f2785866216ab395cdf5
sanjeevz3009 <sanjeevz1717@gmail.com>
2024-04-12 22:50:05 +0100

    OpenCV video feed integrated into the streamlit application

--- sign_language_recog.py
+++ sign_language_recog.py
@@ -6,6 +6,7 @@ import numpy as np
 from tensorflow.keras.callbacks import TensorBoard
 from tensorflow.keras.layers import LSTM, Dense
 from tensorflow.keras.models import Sequential
+import streamlit as st
 
 from gestures_to_detect import gestures
 from utils import (
@@ -37,67 +38,147 @@ model.compile(
 
 model.load_weights("gestures.h5")
 
-sequence = []
-sentence = []
-predictions = []
-threshold = 0.95
+# sequence = []
+# sentence = []
+# predictions = []
+# threshold = 0.95
 
-capture = cv2.VideoCapture(0)
-# Access/ set media pipe mode
-with mediapipe_holistic.Holistic(
-    min_detection_confidence=0.5, min_tracking_confidence=0.5
-) as holistic:
-    while capture.isOpened():
-        ret, frame = capture.read()
+# capture = cv2.VideoCapture(0)
+# # Access/ set media pipe mode
+# with mediapipe_holistic.Holistic(
+#     min_detection_confidence=0.5, min_tracking_confidence=0.5
+# ) as holistic:
+#     while capture.isOpened():
+#         ret, frame = capture.read()
 
-        # Make detections
-        image, results = mediapipe_detection(frame, holistic)
-        print(results)
+#         # Make detections
+#         image, results = mediapipe_detection(frame, holistic)
+#         print(results)
 
-        # Draw landmarks
-        image = draw_landmarks_custom(image, results)
+#         # Draw landmarks
+#         image = draw_landmarks_custom(image, results)
 
-        get_key_points = extract_landmarks(results)
-        sequence.append(get_key_points)
-        sequence = sequence[-30:]
+#         get_key_points = extract_landmarks(results)
+#         sequence.append(get_key_points)
+#         sequence = sequence[-30:]
 
-        # Prediction logic
-        if len(sequence) == 30:
-            res = model.predict(np.expand_dims(sequence, axis=0))[0]
-            print(gestures[np.argmax(res)])
-            predictions.append(np.argmax(res))
+#         # Prediction logic
+#         if len(sequence) == 30:
+#             res = model.predict(np.expand_dims(sequence, axis=0))[0]
+#             print(gestures[np.argmax(res)])
+#             predictions.append(np.argmax(res))
 
-            # Visualation logic
-            if np.unique(predictions[-10:])[0] == np.argmax(res):
-                if res[np.argmax(res)] > threshold:
-                    if len(sentence) > 0:
-                        if gestures[np.argmax(res)] != sentence[-1]:
+#             # Visualation logic
+#             if np.unique(predictions[-10:])[0] == np.argmax(res):
+#                 if res[np.argmax(res)] > threshold:
+#                     if len(sentence) > 0:
+#                         if gestures[np.argmax(res)] != sentence[-1]:
+#                             sentence.append(gestures[np.argmax(res)])
+#                     else:
+#                         sentence.append(gestures[np.argmax(res)])
+
+#             if len(sentence) > 5:
+#                 sentence = sentence[-5:]
+
+#             # Probabilities
+#             image = prob_visualation(res, gestures, image, colours)
+
+#         cv2.rectangle(image, (0, 0), (640, 40), (245, 117, 16), -1)
+#         cv2.putText(
+#             image,
+#             " ".join(sentence),
+#             (3, 30),
+#             cv2.FONT_HERSHEY_SIMPLEX,
+#             1,
+#             (255, 255, 255),
+#             2,
+#             cv2.LINE_AA,
+#         )
+
+#         cv2.imshow("Feed", image)
+
+#         if cv2.waitKey(1) == ord("q"):
+#             break
+
+#     capture.release()
+#     cv2.destroyAllWindows()
+
+
+def main():
+    st.title("Sign Language Recognition")
+    frame_placeholder = st.empty()
+    stop_button_pressed = st.button("Stop")
+
+    sequence = []
+    sentence = []
+    predictions = []
+    threshold = 0.95
+
+    capture = cv2.VideoCapture(0)
+    # Access/ set media pipe mode
+    with mediapipe_holistic.Holistic(
+        min_detection_confidence=0.5, min_tracking_confidence=0.5
+    ) as holistic:
+        while capture.isOpened() and not stop_button_pressed:
+            ret, frame = capture.read()
+
+            if not ret:
+                st.write("The video feed has ended.")
+
+            # Make detections
+            image, results = mediapipe_detection(frame, holistic)
+            print(results)
+
+            # Draw landmarks
+            image = draw_landmarks_custom(image, results)
+
+            get_key_points = extract_landmarks(results)
+            sequence.append(get_key_points)
+            sequence = sequence[-30:]
+
+            # Prediction logic
+            if len(sequence) == 30:
+                res = model.predict(np.expand_dims(sequence, axis=0))[0]
+                print(gestures[np.argmax(res)])
+                predictions.append(np.argmax(res))
+
+                # Visualation logic
+                if np.unique(predictions[-10:])[0] == np.argmax(res):
+                    if res[np.argmax(res)] > threshold:
+                        if len(sentence) > 0:
+                            if gestures[np.argmax(res)] != sentence[-1]:
+                                sentence.append(gestures[np.argmax(res)])
+                        else:
                             sentence.append(gestures[np.argmax(res)])
-                    else:
-                        sentence.append(gestures[np.argmax(res)])
 
-            if len(sentence) > 5:
-                sentence = sentence[-5:]
+                if len(sentence) > 5:
+                    sentence = sentence[-5:]
 
-            # Probabilities
-            image = prob_visualation(res, gestures, image, colours)
+                # Probabilities
+                image = prob_visualation(res, gestures, image, colours)
 
-        cv2.rectangle(image, (0, 0), (640, 40), (245, 117, 16), -1)
-        cv2.putText(
-            image,
-            " ".join(sentence),
-            (3, 30),
-            cv2.FONT_HERSHEY_SIMPLEX,
-            1,
-            (255, 255, 255),
-            2,
-            cv2.LINE_AA,
-        )
+            cv2.rectangle(image, (0, 0), (640, 40), (245, 117, 16), -1)
+            cv2.putText(
+                image,
+                " ".join(sentence),
+                (3, 30),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                1,
+                (255, 255, 255),
+                2,
+                cv2.LINE_AA,
+            )
 
-        cv2.imshow("Feed", image)
+            frame_conversion = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            frame_placeholder.image(frame_conversion, channels="RGB")
 
-        if cv2.waitKey(1) == ord("q"):
-            break
+            # cv2.imshow("Feed", image)
 
-    capture.release()
-    cv2.destroyAllWindows()
+            if cv2.waitKey(1) == ord("q") or stop_button_pressed:
+                break
+
+        capture.release()
+        cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
