c231f033da1898fc9bd8db851018fd43c4a66c0e
sanjeevz3009 <sanjeevz1717@gmail.com>
2024-04-18 00:19:02 +0100

    STUN and TURN server config added

--- sign_language_recog.py
+++ sign_language_recog.py
@@ -20,6 +20,8 @@ from tensorflow.keras.models import Sequential
 from gestures_to_detect import gestures
 from utils import draw_landmarks_custom, extract_landmarks, mediapipe_detection
 
+from twilio_turn_server import get_ice_servers
+
 # Constants
 # These constants define minimum confidence thresholds for detection and tracking
 # model input shape, and log directory
@@ -203,56 +205,43 @@ model.load_weights("gestures_3.h5")
 # if __name__ == "__main__":
 #     main()
 
-# import streamlit as st
-# from streamlit_webrtc import VideoTransformerBase, webrtc_streamer
-# import mediapipe as mp
-# from utils import draw_landmarks_custom, mediapipe_detection
-# import av
-
-# # Holistic model
-# mediapipe_holistic = mp.solutions.holistic
-# st.title("Sign Language Recognition")
-# gesture_text = st.empty()
-# stop_button_pressed = st.button("Stop")
-
-# class HolisticTransformer(VideoTransformerBase):
-#     def __init__(self):
-#         self.holistic = mediapipe_holistic.Holistic(
-#             min_detection_confidence=0.5, min_tracking_confidence=0.5
-#         )
-
-#     def recv(self, frame):
-#         image = frame.to_ndarray(format="bgr24")
-
-#         # Make detections
-#         image, results = mediapipe_detection(image, self.holistic)
-#         print(results)
-
-#         # Draw landmarks
-#         image = draw_landmarks_custom(image, results)
-
-#         return av.VideoFrame.from_ndarray(image, format="bgr24")
-
-# webrtc_ctx = webrtc_streamer(
-#     key="example", 
-#     video_transformer_factory=HolisticTransformer,
-#     async_transform=True,
-# )
-
-# if webrtc_ctx.video_transformer:
-#     if stop_button_pressed:
-#         webrtc_ctx.video_transformer.holistic.close()
-
-from streamlit_webrtc import webrtc_streamer
+import streamlit as st
+from streamlit_webrtc import VideoTransformerBase, webrtc_streamer
+import mediapipe as mp
+from utils import draw_landmarks_custom, mediapipe_detection
 import av
 
+# Holistic model
+mediapipe_holistic = mp.solutions.holistic
+st.title("Sign Language Recognition")
+gesture_text = st.empty()
+stop_button_pressed = st.button("Stop")
 
-def video_frame_callback(frame):
-    img = frame.to_ndarray(format="bgr24")
+class HolisticTransformer(VideoTransformerBase):
+    def __init__(self):
+        self.holistic = mediapipe_holistic.Holistic(
+            min_detection_confidence=0.5, min_tracking_confidence=0.5
+        )
 
-    flipped = img[::-1,:,:]
+    def recv(self, frame):
+        image = frame.to_ndarray(format="bgr24")
 
-    return av.VideoFrame.from_ndarray(flipped, format="bgr24")
+        # Make detections
+        image, results = mediapipe_detection(image, self.holistic)
+        print(results)
 
+        # Draw landmarks
+        image = draw_landmarks_custom(image, results)
 
-webrtc_streamer(key="example", video_frame_callback=video_frame_callback)
+        return av.VideoFrame.from_ndarray(image, format="bgr24")
+
+webrtc_ctx = webrtc_streamer(
+    key="example", 
+    rtc_configuration={"iceServers": get_ice_servers()},
+    video_transformer_factory=HolisticTransformer,
+    async_transform=True,
+)
+
+if webrtc_ctx.video_transformer:
+    if stop_button_pressed:
+        webrtc_ctx.video_transformer.holistic.close()
