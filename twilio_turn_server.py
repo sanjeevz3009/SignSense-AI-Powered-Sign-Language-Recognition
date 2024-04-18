9903cd602b9883fcd14979361ad0e30b713049d8
sanjeevz3009 <sanjeevz1717@gmail.com>
2024-04-18 21:31:59 +0100

    Turn server file updated

--- twilio_turn_server.py
+++ twilio_turn_server.py
@@ -6,34 +6,30 @@ from twilio.rest import Client
 
 logger = logging.getLogger(__name__)
 
+
 @st.cache_data
 def get_ice_servers():
     """Use Twilio's TURN server because Streamlit Community Cloud has changed
-    its infrastructure and WebRTC connection cannot be established without TURN server now.
+    its infrastructure and WebRTC connection cannot be established without TURN server now.  # noqa: E501
+    We considered Open Relay Project (https://www.metered.ca/tools/openrelay/) too,
+    but it is not stable and hardly works as some people reported like https://github.com/aiortc/aiortc/issues/832#issuecomment-1482420656  # noqa: E501
+    See https://github.com/whitphx/streamlit-webrtc/issues/1213
     """
+
     # Ref: https://www.twilio.com/docs/stun-turn/api
     try:
         account_sid = os.environ["TWILIO_ACCOUNT_SID"]
         auth_token = os.environ["TWILIO_AUTH_TOKEN"]
-        print("SuccessSuccessSuccessSuccess")
     except KeyError:
         logger.warning(
-            "Twilio credentials are not set. Fallback to a free STUN server from Google."
+            "Twilio credentials are not set. Fallback to a free STUN server from Google."  # noqa: E501
         )
         return [{"urls": ["stun:stun.l.google.com:19302"]}]
 
-    try:
-        client = Client(account_sid, auth_token)
-        token = client.tokens.create()
-        print("SuccessSuccessSuccessSuccess2")
-        return token.ice_servers
-    except Exception as e:
-        logger.error(f"Failed to retrieve ICE servers from Twilio: {str(e)}")
-        return [{"urls": ["stun:stun.l.google.com:19302"]}]  # Fallback to Google STUN server
+    client = Client(account_sid, auth_token)
+
+    token = client.tokens.create()
+
+    return token.ice_servers
 
 get_ice_servers()
-
-import os
-
-for key, value in os.environ.items():
-    print(f'{key}: {value}')
