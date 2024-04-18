10dfe49992d6fcf26ffec572cf7ca8294f3fa073
sanjeevz3009 <sanjeevz1717@gmail.com>
2024-04-18 22:01:21 +0100

    Testing turn servers

--- twilio_turn_server.py
+++ twilio_turn_server.py
@@ -3,7 +3,6 @@ import os
 
 import streamlit as st
 from twilio.rest import Client
-from twilio.base.exceptions import TwilioRestException
 
 logger = logging.getLogger(__name__)
 
@@ -18,28 +17,20 @@ def get_ice_servers():
     """
 
     # Ref: https://www.twilio.com/docs/stun-turn/api
-    # try:
-    #     account_sid = os.environ["TWILIO_ACCOUNT_SID"]
-    #     auth_token = os.environ["TWILIO_AUTH_TOKEN"]
-    # except KeyError:
-    #     logger.warning(
-    #         "Twilio credentials are not set. Fallback to a free STUN server from Google."  # noqa: E501
-    #     )
-    #     return [{"urls": ["stun:stun.l.google.com:19302"]}]
+    try:
+        account_sid = os.environ["TWILIO_ACCOUNT_SID"]
+        auth_token = os.environ["TWILIO_AUTH_TOKEN"]
+    except KeyError:
+        logger.warning(
+            "Twilio credentials are not set. Fallback to a free STUN server from Google."  # noqa: E501
+        )
+        return [{"urls": ["stun:stun.l.google.com:19302"]}]
 
-    # client = Client(account_sid, auth_token)
-
-    # token = client.tokens.create()
-
-    # return token.ice_servers
-
-    account_sid = os.environ["TWILIO_ACCOUNT_SID"]
-    auth_token = os.environ["TWILIO_AUTH_TOKEN"]
     client = Client(account_sid, auth_token)
 
-    try:
-        token = client.tokens.create()
-        print(token.ice_servers)
-        return token.ice_servers
-    except TwilioRestException as e:
-        print(e)
+    token = client.tokens.create()
+    print(token.ice_servers)
+
+    return token.ice_servers
+
+get_ice_servers()
